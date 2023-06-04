f0356f83ce56d07cff61358c64c9ce51e6c4c903
Vicente Santos <vicentedpsantos@gmail.com>
2023-06-04 13:33:22 +0100

    Display expense items

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
-import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
