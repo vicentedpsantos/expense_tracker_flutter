fc9f8cff09c1356663e803785548a66142975352
Vicente Santos <vicentedpsantos@gmail.com>
2023-06-04 13:07:44 +0100

    Rendering list of expense titles

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,21 +1,42 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/expenses_list.dart';
+
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
   @override
   State<Expenses> createState() {
     return _ExpensesState();
-
   }
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Tibia',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.leisure),
+    Expense(
+        title: 'Cinema',
+        amount: 15.69,
+        date: DateTime.now(),
+        category: Category.leisure),
+    Expense(
+        title: 'McDonalds',
+        amount: 22.00,
+        date: DateTime.now(),
+        category: Category.food)
+  ];
+
   @override
   Widget build(context) {
-    return Scaffold(body: Column(children: [
+    return Scaffold(
+        body: Column(children: [
       Text('The chart'),
-      Text('Expenses list...')
+      Expanded(child: ExpensesList(expenses: _registeredExpenses)),
     ]));
   }
 }
