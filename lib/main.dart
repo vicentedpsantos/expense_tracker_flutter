6141e9fe2f7a61e0835200bf40b823e915822c56
Vicente Santos <vicentedpsantos@gmail.com>
2023-06-04 23:11:03 +0100

    Creating color schemes

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/widgets/expenses.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 240, 17, 91));
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.primaryContainer)),
       home: Expenses(),
     ),
   );
