e4a8b97b74c8cbe874268bc0999c33c3b904eb87
Vicente Santos <vicentedpsantos@gmail.com>
2023-06-04 14:13:36 +0100

    Start creating form for new expense

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -34,7 +35,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => Text('eae'),
+      builder: (ctx) => NewExpense(),
     );
   }
 
